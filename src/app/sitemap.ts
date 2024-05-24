85c38a9ffe8724572128338f3a396c92ad9c466d
umarbashirr <umarbashir93@outlook.com>
2024-05-24 21:51:43 +0530

    add env and sitemap code

---
+++ src/app/sitemap.ts
@@ -0,0 +1,13 @@
+import { routes } from "@/lib/routes";
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const siteMap: any = routes.map((route) => ({
+    url: process.env.NEXT_PUBLIC_APP_URL + route.path,
+    lastModified: new Date(),
+    changeFrequency: "weekly",
+    priority: 1,
+  }));
+
+  return siteMap;
+}
