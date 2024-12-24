53818a9ba446e8f3ad6c1660c33fa85e41d796db
umarbashirr <umarbashir93@outlook.com>
2024-12-24 16:05:09 +0530

    Added Sharp and updated nextjs config for redirect from non www to www

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    if (process.env.NODE_ENV === "production") {
+      return [
+        // Basic redirect
+        {
+          source: "/",
+          has: [
+            {
+              type: "host",
+              value: "theladakhavenue.com", // Replace with your non-www domain
+            },
+          ],
+          destination: "https://www.theladakhavenue.com", // Redirect to the www version
+          permanent: true,
+        },
+      ];
+    }
+    return [];
+  },
 };
 
 export default nextConfig;
