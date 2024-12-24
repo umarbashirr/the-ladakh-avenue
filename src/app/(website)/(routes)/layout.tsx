5feb2cf7b39742141fab0bc15eadecf9d5a75922
umarbashirr <umarbashir93@outlook.com>
2024-12-24 14:46:20 +0530

    add google tag manager

--- src/app/(website)/(routes)/layout.tsx
+++ src/app/(website)/(routes)/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import Header from "../_components/header";
 import Footer from "../_components/footer";
 import ScrollToTop from "../_components/scroll-to-top";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const WebsiteLayout = ({ children }: { children: ReactNode }) => {
   return (
@@ -10,6 +11,7 @@ const WebsiteLayout = ({ children }: { children: ReactNode }) => {
       <main className="min-h-screen">{children}</main>
       <ScrollToTop />
       <Footer />
+      <GoogleTagManager gtmId={process.env.GOOGLE_TAG_MANAGER_ID as string} />
     </div>
   );
 };
