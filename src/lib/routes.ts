902fe7c28a6f4264522dc6ff293575a8a3c84b88
umarbashirr <umarbashir93@outlook.com>
2024-12-24 15:05:32 +0530

    sitemap issue fixed

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -35,7 +35,34 @@ export const routes = [
 ];
 
 export const allRoutes = [
-  ...routes,
+  {
+    label: "Home",
+    path: "/",
+  },
+  {
+    label: "About Us",
+    path: "/about-us",
+  },
+  {
+    label: "Our Rooms",
+    path: "/our-rooms",
+  },
+  {
+    label: "Our Gallery",
+    path: "/gallery",
+  },
+  {
+    label: "Practical Information",
+    path: "/practical-information",
+  },
+  {
+    label: "News & Events",
+    path: "/news-and-events",
+  },
+  {
+    label: "Contact",
+    path: "/contact-us",
+  },
   {
     label: "Privacy Policy",
     path: "/privacy-policy",
